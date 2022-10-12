04e9173754a4ba05f8e87cb3d14fef5d95711941
SkIlLIsSuEs <103995093+Millerrrrrrrrrr@users.noreply.github.com>
2022-10-12 19:16:37 +0100

    Delete Currupt Meteor directory

--- Currupt Meteor/Meteor/Data/NotifyColours.lua
+++
@@ -1,8 +0,0 @@
-local NotifyColours = {}
-
-NotifyColours["red"] = 0xff0000ff
-NotifyColours["blue"] = 0x64FA7800
-NotifyColours["green"] = 0x00ff00
-NotifyColours["orange"] = 0xff0080ff
-
-return NotifyColours
