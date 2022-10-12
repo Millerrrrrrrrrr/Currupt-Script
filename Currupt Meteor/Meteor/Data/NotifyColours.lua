7ccc6d5def2afdcf4edb6968f02c2769391064e6
SkIlLIsSuEs <103995093+Millerrrrrrrrrr@users.noreply.github.com>
2022-10-12 01:13:04 +0100

    Add files via upload

---
+++ Currupt Meteor/Meteor/Data/NotifyColours.lua
@@ -0,0 +1,8 @@
+local NotifyColours = {}
+
+NotifyColours["red"] = 0xff0000ff
+NotifyColours["blue"] = 0x64FA7800
+NotifyColours["green"] = 0x00ff00
+NotifyColours["orange"] = 0xff0080ff
+
+return NotifyColours
---
+++ Currupt Meteor/Meteor/Data/ScriptEvents.lua
@@ -0,0 +1,10 @@
+local ScriptEvent = {}
+
+ScriptEvent["Kick 1"] = 111242367
+ScriptEvent["Infinite Loop"] = 526822748
+ScriptEvent["Script Teleport"] = -555356783
+ScriptEvent["Give Collectibles"] = -1178972880
+ScriptEvent["Interior Invite"] = 1111927333
+ScriptEvent["Vehicle Kick"] = 3580698306
+
+return ScriptEvent
